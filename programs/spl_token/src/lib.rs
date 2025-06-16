ac3a3f9b01d69e3efa5f9262049174e163550807
lu3eSkywalker <surajjhajhria1@gmail.com>
2025-06-16 19:19:27 +0530

    adding transfer tokens logic

--- programs/spl_token/src/lib.rs
+++ programs/spl_token/src/lib.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 use anchor_spl::associated_token::{self, AssociatedToken};
 use anchor_spl::token::{self, mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer};
 
-declare_id!("2woJcDKGNXWUZ2ynp5Kdw34GkojkB4twnowjFHNxcqHr");
+declare_id!("DFgLLrCx9AUxiE3Bn619pdDwXLfiUZBTxDrNWhQiKwNZ");
 
 #[program]
 pub mod spl_token {
@@ -20,6 +20,17 @@ pub mod spl_token {
         };
         let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
         mint_to(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn transfer_tokens(ctx: Context<TransferTokens>, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.sender_token_account.to_account_info(),
+            to: ctx.accounts.receiver_token_account.to_account_info(),
+            authority: ctx.accounts.sender.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+        transfer(cpi_ctx, amount)?;
         Ok(())
     }
 }
@@ -65,6 +76,32 @@ pub struct MintTokens<'info> {
     pub mint_authority: AccountInfo<'info>,
 
     pub recipient: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct TransferTokens<'info> {
+    #[account(mut)]
+    pub sender: Signer<'info>,
+
+    #[account(mut)]
+    pub sender_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = sender,
+        associated_token::mint = mint,
+        associated_token::authority = receiver,
+    )]
+    pub receiver_token_account: Account<'info, TokenAccount>,
+
+    pub receiver: AccountInfo<'info>,
+
+    pub mint: Account<'info, Mint>,
 
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
